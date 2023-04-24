545cb3960741a143f7d38c3de1959052fa1b54b5
RodolfoJGonz <rodolfojgs@hotmail.com>
2023-04-24 08:36:05 -0500

    addint createSave updateSave Version1

--- src/main/java/com/project/savefiles.java
+++ src/main/java/com/project/savefiles.java
@@ -2,27 +2,38 @@ package com.project;
 
 import java.sql.*;
 
+
 public class savefiles 
 {
+    static final String QUERY = "SELECT name, key, chars FROM Userdata";
 
     public Connection connect() throws SQLException
     {
         return DriverManager.getConnection("jdbc:sqlite:userdata.db");
     }
 
-    
-    // public void displayRecords(Connection conn) throws SQLException
-    // {
-    //     Statement st = conn.createStatement();
+    public void createSave(Connection conn, String name, int chars) throws SQLException
+    {
+        Statement st = conn.createStatement();
+        
+        String sql = "INSERT INTO Userdata (name, chars) VALUES (%s, %d)".formatted(name, chars);
+        ResultSet rs = st.executeQuery(sql);
 
-    //     ResultSet rs = st.executeQuery("SELECT * FROM Userdata");
+        rs.close();
+    }
 
-    //     while (rs.next())
-    //     {
-    //         System.out.println("Name: " + rs.getString("name"));
-    //         System.out.println("key: "  + rs.getInt("key"));
-    //     }
-    // }
+    public void updateSave(Connection conn, String name, int chars) throws SQLException
+    {
+        Statement st = conn.createStatement();
+
+        String sql = "UPDATE Userdata SET chars = %d WHERE name in (%s)".formatted(chars, name);
+        st.executeUpdate(sql);
+
+        ResultSet rs = st.executeQuery(QUERY);
+
+        rs.close();
+    }
+
 
     
 }
